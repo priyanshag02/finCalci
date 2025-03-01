86dcb05f99cc2ba0190b980d6004f62a6c30caef
Priyansh Agarwal <187436469+priyanshag02@users.noreply.github.com>
2025-03-02 04:00:05 +0530

    Add files via upload

---
+++ FinCalci/FinCalci.swift
@@ -0,0 +1,17 @@
+//
+//  WealthifyApp.swift
+//  Wealthify
+//
+//  Created by Priyansh on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FinCalci: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
